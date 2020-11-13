ad5360b6503d72975915e7ebc3263c5ebe71a685
Thuy <thuy121997@yahoo.com>
2020-11-13 13:58:01 -0500

    debugged game_config

--- game_config.py
+++ game_config.py
@@ -1,11 +1,11 @@
 import os
 from pygame import image, transform
 
-big_matched = image.load('First Project/ending/matched.png')
+big_matched = image.load('ending/matched.png')
 small_matched = big_matched.copy()
 matched = transform.scale(small_matched, (512,512))
 
-congrats_1 = image.load('First Project/ending/congrats.png')
+congrats_1 = image.load('ending/congrats.png')
 congrats = congrats_1.copy()
 congrats = transform.scale(congrats, (512,512))
 
@@ -15,7 +15,7 @@ num_tiles_side = 4
 num_tiles_total = 16
 margin = 4
 
-asset_dir = 'First Project/assets'
+asset_dir = 'assets'
 asset_files = [x for x in os.listdir(asset_dir) if x[-3:] == 'png']
 
 assert len(asset_files) == 8
